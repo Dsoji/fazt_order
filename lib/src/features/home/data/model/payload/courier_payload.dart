44bbc37165f48d661bf81d59860edc317bb7abda
Oluwadamilola Olasoji <dami.osoj@gmail.com>
2025-08-29 05:40:03 +0100

    chore: updated courier payload

--- lib/src/features/home/data/model/payload/courier_payload.dart
+++ lib/src/features/home/data/model/payload/courier_payload.dart
@@ -17,9 +17,29 @@ class CourierPayload extends MapView<String, dynamic> {
     String? senderName,
     String? senderEmail,
     String? senderPhone,
+    String? deliveryAddressCity,
+    String? deliveryAddressState,
+    String? deliveryAddressLong,
+    String? deliveryAddressLat,
+    String? pickUpAddressCity,
+    String? pickUpAddressState,
+    String? pickUpAddressLong,
+    String? pickUpAddressLat,
   }) : super({
-          "deliveryAddress": deliveryAddress,
-          "pickUpAddress": pickUpAddress,
+          "deliveryAddress": {
+            "address": deliveryAddress,
+            "city": deliveryAddressCity,
+            "state": deliveryAddressState,
+            "long": deliveryAddressLong,
+            "lat": deliveryAddressLat
+          },
+          "pickUpAddress": {
+            "address": pickUpAddress,
+            "city": pickUpAddressCity,
+            "state": pickUpAddressState,
+            "long": pickUpAddressLong,
+            "lat": pickUpAddressLat
+          },
           "parcelType": parcelType,
           "instructions": instructions,
           "receiverInfo": {
